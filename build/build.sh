d5da1bab78ea2b99227deade9ff46bfa437290d8
fjboy <fjboy.z@outlook.com>
2023-06-01 00:23:52 +0800

    优化build脚本
    Signed-off-by: fjboy <fjboy.z@outlook.com>

--- build/build.sh
+++ build/build.sh
@@ -2,30 +2,10 @@
 function logInfo() {
     echo "INFO:" $@ 1>&2
 }
-function getVersionByGit(){
-    tagRage=HEAD
-    lastTag=$(git tag --sort=-taggerdate |tail -n1)
-    if [[ "${lastTag}" != "" ]]; then
-        tagRange="${tagRange...${lastTag}}"
-    else
-        lastTag="0.0.0"
-    fi
-
-    commitNum=$(git log --pretty=oneline ${tagRange} |wc -l)
-    logInfo "last tag is ${lastTag}, commit num is ${commitNum}"
-
-    if [[ ${commitNum} -eq 0 ]]; then
-        version="${lastTag}"
-    else
-        version="${lastTag}.dev${commitNum}"
-    fi
-    echo ${version}
-}
 
 function main(){
-    version=$(getVersionByGit)
+    version=$(go run cmd/magic-pocket.go version)
     logInfo "set main.Version to ${version}"  1>&2
     go build  -ldflags "-X main.Version=${version}" cmd/magic-pocket.go
 }
-
 main
--- cmd/commands/version.go
+++ cmd/commands/version.go
@@ -12,7 +12,7 @@ func GetVersionCommand() cobra.Command {
 
 	var command = &cobra.Command{
 		Use:   "version",
-		Short: "版本",
+		Short: "获取版本",
 		Long:  "获取工具版本",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println(Version)
