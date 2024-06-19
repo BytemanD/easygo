b69d5a2bad7fe47b61e3e4e6fb2ede259c0c31db
BytemanD <byteman.e@outlook.com>
2024-06-19 23:02:20 +0800

    feat: compare包添加更多方法
    Signed-off-by: BytemanD <byteman.e@outlook.com>

--- pkg/compare/type.go
+++ pkg/compare/type.go
@@ -1,5 +1,9 @@
 package compare
 
+import (
+	"reflect"
+)
+
 func IsType[T any](i interface{}) bool {
 	switch i.(type) {
 	case T:
@@ -7,4 +11,45 @@ func IsType[T any](i interface{}) bool {
 	default:
 		return false
 	}
+}
+
+func IsStructOf(structName string, v1 interface{}) bool {
+	if v1 == nil {
+		return false
+	}
+	if reflect.ValueOf(v1).Type().Name() == structName {
+		return true
+	}
+	return IsPtrOf(structName, v1)
+}
+
+func IsPtrOf(structName string, v1 interface{}) bool {
+	if v1 == nil {
+		return false
+	}
+	v1Value := reflect.ValueOf(v1)
+	if v1Value.Kind() != reflect.Ptr {
+		return false
+	}
+	v1ElemType := v1Value.Elem().Type()
+	if v1ElemType.Kind() != reflect.Struct {
+		return false
+	}
+	return v1ElemType.Name() == structName
+}
+
+func EqualsStruct(v1 interface{}, v2 interface{}) bool {
+	if v1 == nil {
+		return v2 == nil
+	}
+
+	v1Value := reflect.ValueOf(v1)
+	var v1TypeName string
+	if v1Value.Kind() == reflect.Ptr {
+		v1TypeName = v1Value.Elem().Type().Name()
+	} else {
+		v1TypeName = reflect.ValueOf(v1).Type().Name()
+	}
+
+	return IsStructOf(v1TypeName, v2)
 }
--- pkg/compare/type_test.go
+++ pkg/compare/type_test.go
@@ -26,6 +26,25 @@ func TestIsTypeError(t *testing.T) {
 	}
 }
 
+type myError struct {
+	Message string
+}
+
+func (e myError) Error() string {
+	return e.Message
+}
+
+func TestIsTypeCustomError(t *testing.T) {
+	if IsType[myError](myError{Message: "error"}) != true {
+		t.Errorf(`IsType[MyError](MyError{Message: "error"}) is false`)
+		return
+	}
+	if IsType[myError](fmt.Errorf("error")) != false {
+		t.Errorf(`IsType[MyError](fmt.Errorf("error")) is true`)
+		return
+	}
+}
+
 type demoStruct struct {
 	Id string
 }
@@ -37,6 +56,64 @@ func TestIsTypeStruct(t *testing.T) {
 	}
 	if IsType[demoStruct](1) != false {
 		t.Errorf(`IsType[demoStruct](1) is true`)
+		return
+	}
+}
+
+func TestIsStructOf(t *testing.T) {
+	if IsStructOf("demoStruct", demoStruct{}) != true {
+		t.Errorf(`IsStructOf(demoStruct, demoStruct{}) is false`)
+		return
+	}
+	if IsStructOf("demoStruct", &demoStruct{}) != true {
+		t.Errorf(`IsStructOf(demoStruct, &demoStruct{}) is false`)
+		return
+	}
+	if IsStructOf("demoStruct", myError{}) != false {
+		t.Errorf(`IsStructOf(demoStruct, myError{}) is true`)
+		return
+	}
+	if IsStructOf("demoStruct", "demoStruct") != false {
+		t.Errorf(`IsStructOf(demoStruct, "demoStruct) is true`)
+		return
+	}
+	if IsStructOf("demoStruct", nil) != false {
+		t.Errorf(`IsStructOf(demoStruct, "demoStruct) is true`)
+		return
+	}
+}
+
+func TestEqualsStruct(t *testing.T) {
+	if EqualsStruct(nil, nil) != true {
+		t.Errorf(`EqualsStruct(nil, nil) is false`)
+		return
+	}
+	if EqualsStruct(nil, demoStruct{}) != false {
+		t.Errorf(`EqualsStruct(nil, demoStruct{}) is true`)
+		return
+	}
+	if EqualsStruct(demoStruct{}, nil) != false {
+		t.Errorf(`EqualsStruct(demoStruct{}, nil) is true`)
+		return
+	}
+	if EqualsStruct(demoStruct{}, demoStruct{}) != true {
+		t.Errorf(`EqualsStruct(demoStruct{}, demoStruct{}) is false`)
+		return
+	}
+	if EqualsStruct(demoStruct{}, &demoStruct{}) != true {
+		t.Errorf(`EqualsStruct(demoStruct{}, &demoStruct{}) is false`)
+		return
+	}
+	if EqualsStruct(&demoStruct{}, demoStruct{}) != true {
+		t.Errorf(`EqualsStruct(&demoStruct{}, demoStruct{}) is false`)
+		return
+	}
+	if EqualsStruct(&demoStruct{}, &demoStruct{}) != true {
+		t.Errorf(`EqualsStruct(&demoStruct{}, &demoStruct{}) is false`)
+		return
+	}
+	if EqualsStruct(demoStruct{}, myError{}) != false {
+		t.Errorf(`EqualsStruct(demoStruct{}, myError{}) is true`)
 		return
 	}
 }
